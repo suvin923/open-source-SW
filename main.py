8d603e41615933d6f3087b7f646800a632d433e6
suvin923 <163493987+suvin923@users.noreply.github.com>
2024-03-28 19:36:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ touched by HUFSSV
 h = float(input("Enter the height of cylinder: "))
 r = float(input("Enter the radius of cylinder: "))
 pi = 3.1416
-tsa = 2*pi(r**2+r*h)
+tsa = 2*pi*(r**2+r*h)
 print(tse)
